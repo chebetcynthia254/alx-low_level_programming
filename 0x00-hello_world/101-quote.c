a9f29e36559ad78dd36550be717cab4a3fe8169b
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-13 06:23:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 /**
 * main - Entry point
-*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+CStr  message = "and that piece of art is useful\" -*Dora Korpar, 2015-10-19\n";
+ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
+if (bytes_written == -1)
+{
+write(STDERR_FILENO, ERR_MSG, sizeof(ERR_MSG) - 1);
 return (1);
 }
+return (1);
 }
+
