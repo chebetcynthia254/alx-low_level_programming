103b39f27b0cdd195f78e409e6aa5dc518843493
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 11:57:23 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,21 +6,22 @@
 */
 int main(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
+int num1;
+int num2;
+int num3;
+for (num1 = 0; num1 < 9; num1++)
 {
-for (j = i + 1; j < 10; j++)
+for (num2 = num1 + 1; num2 < 9; num2++)
 {
-for (k = j + 1; k < 10; k++)
+for (num3 = num2 + 1; num3 < 10; num3++)
 {
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
-{
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
+putchar((num3 % 10) + '0');
+if (num1 == 7 && num2 == 8 && num3 == 9)
+continue;
 putchar(',');
-utchar(' ');
-}
+putchar(' ');
 }
 }
 }
