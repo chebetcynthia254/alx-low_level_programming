d61e8462dd2dba368f6aacbf62e41b48dc27e47f
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 01:02:24 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar('a' + i);
+}
+putchar('\n');
+return (0);
+}
