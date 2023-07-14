9395d5f880abbbbc8ce2bdcfc6720f4790faeedf
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 08:13:32 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,11 @@
 */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
-putchar('\n');
+int n;
+for (n = 0; n >= 9; n++)
+{
+puchar(n + 48);
+}
+putchar("\n");
 return (0);
 }
