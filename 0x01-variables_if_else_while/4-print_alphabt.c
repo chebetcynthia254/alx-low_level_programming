f746a7d2942f14131166bb4f323dab91b7f292d9
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 07:28:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+}
+putchar('\n');
+return (0);
+}
