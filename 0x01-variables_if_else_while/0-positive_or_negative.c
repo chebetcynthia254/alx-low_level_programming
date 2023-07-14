2f953f89e67297aa4c361736788dd8146e15474a
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 02:52:59 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+printf("98 positive\n");
 }
 else if (n == 0)
 {
-printf("is zero\n");
+printf("0 is zero\n");
 }
 else
 {
-printf("is negative\n");
+printf("-98 is negative\n");
 }
 return (0);
 }
