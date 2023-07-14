bc3827eb9ea53fb9b7b938e772e5c52eabdf7ec7
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-14 07:40:05 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+utchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
+putchar('\n');
+return (0);
+}
