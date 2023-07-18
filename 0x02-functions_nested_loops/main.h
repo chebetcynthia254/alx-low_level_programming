dc4651f51e48a87825031e671aafec6d6415611b
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-18 10:10:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-0 int _putchar(char c);
-1 void print_alphabet(void);
-2 void print_alphabet_x10(void);
-3 int _islower(int c);
+#ifndef main_h
+#define main_h
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
