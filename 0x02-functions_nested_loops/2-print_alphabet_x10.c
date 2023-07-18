4b3855406fbdd80646980a3b14772a83c8a1c10a
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-18 07:36:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,14 @@
 * main - check the code.
 * Return: Always 0.
 */
-void print_alphabet_x10(void) {
+void print_alphabet_x10(void)
+{
 int count;
-for (count = 0; count < 10; count++) {
+for (count = 0; count < 10; count++)
+{
 char letter = 'a';
-while (letter <= 'z') {
+while (letter <= 'z')
+{
 putchar(letter);
 letter++;
 }
