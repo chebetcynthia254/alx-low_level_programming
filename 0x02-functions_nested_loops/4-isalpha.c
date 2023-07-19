1883680b45cefe40f0d35e1926bfa890c1033e4f
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-19 02:45:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isalpha -shows 1 if the input is a
- * letter another cases shows 0
- *c: An integer representing the ASCII value of the character to be checked.
- * Return:1 for letters 0 for the rest
- */
+*_isalpha - shows 1 if the input is a
+*letter Another character shows 0
+*c: the character in ANCII code
+*Return: 1 for letters o for the rest
+*/
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
