91b2093dc9415d78b7038e47aafb8bb6a02b66f4
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-19 06:13:05 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,25 @@
 void print_to_98(int n)
 {
 int i;
+int j;
 if (n <= 98)
 {
 for (i = n; i < 98; i++)
 {
-printf("%d, ", i);
+	if (i != 98)
+	printf("%d, ", i);
+	else if (i == 98)
+		printf("%d\n", i);
 }
 }
-else
+else if (n >= 98)
 {
-for (i = n; i > 98; i--)
-{
-printf("%d, ", i);
+	for (j = n; j >= 98; j--)
+	{
+		if (j != 98)
+			printf("%d, ", j);
+		else if (j == 98)
+			printf("%d\n", j);
+	}
 }
-}
-printf("%d\n", 98);
 }
