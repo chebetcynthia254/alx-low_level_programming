9d2353bb93ed68726924b9d50cb02f5eb4bfb7f9
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-20 19:48:43 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,8 +11,8 @@ int main(void)
 long x, maxf;
 
 long number = 612852475143;
-double square = sqrt (number);
-for(x = 1; x <= square; x++)
+double square = sqrt(number);
+for (x = 1; x <= square; x++)
 {
 if (number % x == 0)
 {
