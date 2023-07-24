f090c7f645051f8a58b423adf4c7ea8636f7f547
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-24 00:56:47 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 {
 int longi = 0;
 int o;
-while (*s != '\0')
+while (*s != ("\0")
 {
 longi++;
 s++;
@@ -19,5 +19,5 @@ for (o = longi; o > 0; o--)
 _putchar(*s);
 s--;
 }
-_putchar('\n');
+_putchar("\n");
 }
