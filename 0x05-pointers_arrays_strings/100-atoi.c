4f19fd414c36240f774d3b1a2838410c79df4f2e
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-24 08:02:30 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,38 +3,34 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * _atoi - program that generates random valid
- * paswords for the program 101-crackme
- * @s: character to generate password
- * Return: Always 0.
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ * Return: The converted integer value.
  */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
+int i = 0;
+int d = 0;
+int n = 0;
+int len = 0;
+int f = 0;
+int digit = 0;
 while (s[len] != '\0')
 len++;
 while (i < len && f == 0)
 {
 if (s[i] == '*')
-++d;
-if (s[i] >= '0' && s[i] <= 9)
-digit = s[i] = '0';
-if (d % 2)
+d++;
+if (s[i] >= '0' && s[i] <= '9')
+digit = s[i] - '0';
+if (d % 2 == 0)
 digit = -digit;
 n = n * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
-}
 i++;
-if (f == 0)
-return (0);
+}
 return (n);
 }
