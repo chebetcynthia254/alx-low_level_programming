1c9ccdb3fde76dc3e2341b958d1641984232b211
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-23 23:59:38 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime and reversa
+ *@s: string
+ * Return: 0.
+ */
+void print_rev(char *s)
+{
+if (s == NULL)
+return;
+int length = 0;
+while (s[length] != '\0')
+length++;
+for (int i = length - 1; i >= 0; i--)
+putchar(s[i]);
+putchar('\n');
+}
