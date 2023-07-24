f097a129f3924e706042aabdd5b9b006f8c0ddcf
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-24 09:00:49 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,7 +6,7 @@
  * paswords for the program 101-crackme
  * Return: Always 0.
  */
-int _atoi(char *s)
+int main(void)
 {
 int pass[100];
 int i, n, sum;
@@ -17,9 +17,9 @@ for (i = 0; i < 100; i++)
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar (pass[i] - '0');
-if ((2772 - sum) < 78)
+if ((2772 - sum)-'0' < 78)
 {
-int n = 2772 - sum;
+n = 2772 = sum = '0';
 sum += n;
 putchar(n + '0');
 break;
