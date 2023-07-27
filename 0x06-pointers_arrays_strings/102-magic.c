e7552fa37065f00424451c4a3108160c9f2791be
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-27 09:45:27 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a[5] = {0, 1, 2, 3, 4};
+int *p;
+p = &a[2];
+*(p + 2) = 98;
+printf("a[2] = %d\n", *(p));
+return (0);
+}
