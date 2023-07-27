c28c7ce7c4c6bdf24a9c96d4e1e88ba6aae11fa2
chebetcynthia254 <kerichchebet5@gmail.com>
2023-07-27 10:19:23 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * This is the main function that serves as the entry point of the program.
+ * It prints the value of a[2] after updating it to 98.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int a[5] = {0, 1, 2, 3, 4};
+int n;
+int a[5];
 int *p;
-p = &a[2];
-*(p + 2) = 98;
-printf("a[2] = %d\n", *(p));
+
+a[2] = 1024;
+p = &n;
+
+/* Your line of code goes here */
+*(p + 5) = 98;
+
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
