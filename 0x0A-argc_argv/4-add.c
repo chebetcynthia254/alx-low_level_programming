0ff979378c83642f27e8d8de9a642e0507deb377
chebetcynthia254 <kerichchebet5@gmail.com>
2023-08-04 15:10:17 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,40 +2,56 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
 /**
- * is_positive_number - checks if a string is a positive number
- * @s: string to check
- * Return: 1 if positive number, 0 if not
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
  */
-int is_positive_number(char *s)
+int check_num(char *str)
 {
-while (*s != '\0')
+/*Declaring variables*/
+unsigned int count;
+count = 0;
+while (count < strlen(str)) /*count string*/
 {
-if (!isdigit(*s))
+if (!isdigit(str[count])) /*check if str there are digit*/
+{
 return (0);
-s++;
+}
+count++;
 }
 return (1);
 }
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
 int main(int argc, char *argv[])
 {
-int i;
+/*Declaring variables*/
+int count;
+int str_to_int;
 int sum = 0;
-if (argc < 2)
+count = 1;
+while (count < argc) /*Goes through the whole array*/
 {
-printf("0\n");
-return (0);
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
 }
-for (i = 1; i < argc; i++)
-{
-if (!is_positive_number(argv[i]))
+/*condition if one of the number contains symbols that are are not digits*/
+else
 {
 printf("Error\n");
 return (1);
 }
-sum += atoi(argv[i]);
+count++;
 }
-printf("%d\n", sum);
+printf("%d\n", sum); /*print sum*/
 return (0);
 }
