564016d275e31e0a06d9229c11159fa1c5d38eff
chebetcynthia254 <kerichchebet5@gmail.com>
2023-08-10 01:48:18 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void errors(void);
+int strlen(char *s);
+int is_digit(char *s);
+
+#endif
